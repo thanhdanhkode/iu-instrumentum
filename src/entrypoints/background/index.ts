ba8471509faaf6f9cda4a2aa207b36776ad850fb
thanhdanhkode <bfffamilk@gmail.com>
2025-11-06 21:32:47 +0700

    fix: fix background script

--- src/entrypoints/background/index.ts
+++ src/entrypoints/background/index.ts
@@ -1,30 +1,8 @@
-import { defineBackground, browser } from "#imports"
+import { defineBackground } from "#imports"
+import { onMessage } from "@/utils/message"
 
 export default defineBackground(() => {
-  // browser.action.onClicked.addListener(async (tab) => {
-  //   await browser.sidePanel.open({ windowId: tab.windowId })
-  // })
-
-  browser.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error))
-
-  browser.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-    if (!tab.url) return
-
-    const url = new URL(tab.url)
-
-    console.log(url.origin)
-
-    if (url.origin === "https://edusoftweb.hcmiu.edu.vn") {
-      await browser.sidePanel.setOptions({
-        tabId,
-        path: "sidepanel.html",
-        enabled: true,
-      })
-    } else {
-      await browser.sidePanel.setOptions({
-        tabId,
-        enabled: false,
-      })
-    }
+  onMessage("getAutoLogin", (message) => {
+    return true
   })
 })
